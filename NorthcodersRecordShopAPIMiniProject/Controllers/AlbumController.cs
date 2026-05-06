844e98c6703fb34a672de4c3d15d24c80f47a593
yumna <yumnaeltaba@gmail.com>
2026-05-06 11:09:44 +0100

    created the controller layer for album

---
+++ NorthcodersRecordShopAPIMiniProject/Controllers/AlbumController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using NorthcodersRecordShopAPIMiniProject.Models;
+using NorthcodersRecordShopAPIMiniProject.Services;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace NorthcodersRecordShopAPIMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        private readonly AlbumService _albumService;
+
+        public AlbumController(AlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+
+        [HttpGet]
+        [OutputCache]
+        public List<Album> GetAllAlbums()
+        {
+            return _albumService.GetAllAlbums();
+        }
+    }
+}
