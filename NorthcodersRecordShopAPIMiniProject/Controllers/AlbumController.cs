351a0cd419869ef85f56bb4a4fa70da3c8152fb6
yumna <yumnaeltaba@gmail.com>
2026-05-06 12:30:28 +0100

    can see in swagger

--- NorthcodersRecordShopAPIMiniProject/Controllers/AlbumController.cs
+++ NorthcodersRecordShopAPIMiniProject/Controllers/AlbumController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 using NorthcodersRecordShopAPIMiniProject.Models;
 using NorthcodersRecordShopAPIMiniProject.Services;
-using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
 
 namespace NorthcodersRecordShopAPIMiniProject.Controllers
 {
--- NorthcodersRecordShopAPIMiniProject/Services/AlbumService.cs
+++ NorthcodersRecordShopAPIMiniProject/Services/AlbumService.cs
@@ -24,5 +24,6 @@ namespace NorthcodersRecordShopAPIMiniProject.Services
         {
             return _albumModel.GetAlbumById(id);
         }
+       
     }
 }
