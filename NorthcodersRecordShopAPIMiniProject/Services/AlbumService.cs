b994f77a30ae7dd463cb2702998932bd844a5414
yumna <yumnaeltaba@gmail.com>
2026-05-06 11:06:59 +0100

    created albumservice layer with albumservice interface

---
+++ NorthcodersRecordShopAPIMiniProject/Services/AlbumService.cs
@@ -0,0 +1,28 @@
+﻿using NorthcodersRecordShopAPIMiniProject.Models;
+using NorthcodersRecordShopAPIMiniProject.Repositories;
+
+namespace NorthcodersRecordShopAPIMiniProject.Services
+{
+    public interface IAlbumService
+    {
+        List<Album> GetAllAlbums();
+        Album GetAlbumById(int id);
+    }
+    public class AlbumService : IAlbumService
+    {
+        private readonly IAlbumModel _albumModel;
+
+        public AlbumService(IAlbumModel albumModel)
+        {
+            _albumModel = albumModel;
+        }
+        public List<Album> GetAllAlbums()
+        {
+              return _albumModel.GetAllAlbums();
+        }
+        public Album GetAlbumById(int id)
+        {
+            return _albumModel.GetAlbumById(id);
+        }
+    }
+}
